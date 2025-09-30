45a8b6177e5a1f6f7d28177297694f8f7261df4d
Khalil Hersi <khalil.hersi@hotmail.co.uk>
2025-09-30 17:48:00 +0100

    fixed depricated 'message' bit in s3 bucket resource.

--- resources/s3.tf
+++ resources/s3.tf
@@ -1,8 +1,18 @@
 resource "aws_s3_bucket" "dashboard" {  
   bucket = "dashboard-bucket"
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-  }
   // S3 bucket for storing the dashboard
+}
+
+resource "aws_s3_bucket_website_configuration" "dashboard" {
+  bucket = aws_s3_bucket.dashboard.id
+
+  index_document {
+    suffix = "index.html" 
+    // Index document for the website (this is has been done, find it in s3_dashboard folder)
+  }
+
+  error_document {
+    key = "error.html"
+    // Error document for the website (not done yet)
+  }
 }
