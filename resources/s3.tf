78babb84912dc2c6983cb6aec7750814af9c70e3
Khalil Hersi <khalil.hersi@hotmail.co.uk>
2025-09-30 20:59:05 +0100

    changed index.hmtl location

--- resources/s3.tf
+++ resources/s3.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket_website_configuration" "dashboard" {
   bucket = aws_s3_bucket.dashboard.id
 
   index_document {
-    suffix = "index.html" 
+    suffix = "s3_dashboard/index.html"
     // Index document for the website (this is has been done, find it in s3_dashboard folder)
   }
 
