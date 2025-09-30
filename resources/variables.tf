0466911f88327c13459b532ce77ad6fd08ce1779
Khalil Hersi <khalil.hersi@hotmail.co.uk>
2025-09-30 15:58:30 +0100

    changed desciprion position

--- resources/variables.tf
+++ resources/variables.tf
@@ -1,10 +1,11 @@
 resource "aws_sns_topic" "cost_alerts" {
   name = "cost_alerts"
-  
-}
+  // SNS topic for cost alerts
+} 
 
 resource "aws_sns_topic_subscription" "cost_alerts_email" {
     topic_arn = aws_sns_topic.cost_alerts.arn
     protocol  = "email"
     endpoint  = var.alert_email
-}
+    // Subscription to the SNS topic for email alerts
+} 
