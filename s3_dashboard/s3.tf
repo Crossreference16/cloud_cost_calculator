49f89291e03a58f5e4e3fdcaf29b306580e5c9b0
Khalil Hersi <khalil.hersi@hotmail.co.uk>
2025-09-30 17:17:00 +0100

    another typo fixed in s3

--- s3_dashboard/s3.tf
+++ s3_dashboard/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "dashboard" {  
-  bucket = "dasboard-bucket"
+  bucket = "dashboard-bucket"
   website {
     index_document = "index.html"
     error_document = "error.html"
